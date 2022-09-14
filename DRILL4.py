b08780d5632fa570e1f97a6194347eaae9a3bcd9
none <none@gmail.com>
2022-09-14 17:01:00 +0900

    Create DRILL4.py

---
+++ DRILL4.py
@@ -0,0 +1,29 @@
+import turtle
+
+x = 400
+y = 100
+
+turtle.forward(500)
+turtle.left(90)
+turtle.forward(500)
+turtle.left(90)
+turtle.forward(500)
+turtle.left(90)
+turtle.forward(500)
+turtle.left(90)
+
+while x > 0:
+    turtle.penup()
+    turtle.goto(0, x)
+    turtle.pendown()
+    turtle.forward(500)
+    x -= 100
+
+turtle.setheading(270)
+
+while y < 500:
+    turtle.penup()
+    turtle.goto(y, 500)
+    turtle.pendown()
+    turtle.forward(500)
+    y += 100
