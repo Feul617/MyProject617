b416a5cc9ae2ce4b51ecb8abe74649851f3fe4b4
Feul617 <ho0papa617@gmail.com>
2022-10-13 00:55:57 +0900

    10번 11번 추가
    11번 내용
    로고, 타이틀, 게임화면 전환
    프레임워크 실습

--- DRILL10/boy_grass_object.py
+++ DRILL10/boy_grass_object.py
@@ -1,24 +1,28 @@
+import random
 from pico2d import *
 
+# Game object class here
+
 class Grass:
-    def __init__(self):
+    def __init__(self):  #생성자 -> 객체가 가지고 있는 속성을 정하고 설정
         self.image = load_image('grass.png')
 
     def draw(self):
         self.image.draw(400, 30)
 
+
 class Boy:
     def __init__(self):
-        self.x, self.y = 0, 90
-        self.frame = 0
         self.image = load_image('run_animation.png')
+        self.x, self.y = random.randint(100, 700), 90
+        self.frame = random.randint(0, 7)
 
-    def update(self):
+    def update(self):  # 소년의 행위 구현
+        self.x += 5  # 속성값을 바꿈으로써, 행위(오른쪽으로 이동)을 구현
         self.frame = (self.frame + 1) % 8
-        self.x += 1
 
     def draw(self):
-        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
 
 
 def handle_events():
@@ -30,25 +34,34 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
+# initialization code
 open_canvas()
 
+grass = Grass()  # 잔디 객체를 생성
+#boy = Boy()  # 소년 객체를 생성
 
-boy = Boy()
-grass = Grass()
+team = [Boy() for i in range(11)]
 running = True
 
 # game main loop code
+
 while running:
-    handle_events()
+    handle_events() # 키 입력을 받아들이는 처리
 
-    boy.update()
+    for boy in team:
+        boy.update()
+    # Game logic
 
+
+
+    # Game Drawing
     clear_canvas()
     grass.draw()
-    boy.draw()
+    for boy in team:
+        boy.draw()
     update_canvas()
 
     delay(0.05)
 
+
 # finalization code
-close_canvas()
